e12ddaeab29dfb858402d6940c7b7c8d437d3879
Draque <gauciboris@live.fr>
2016-02-16 12:42:13 -0500

    modif run + script d'install auto

---
+++ install_auto.sh
@@ -0,0 +1,8 @@
+gem install selenium-webdriver
+apt-get update
+apt-get install default-jre
+cd ..
+wget http://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar
+java -jar selenium-server-standalone-2.52.0.jar &
+cd -
+./run
--- run.rb
+++ run.rb
@@ -2,15 +2,36 @@
 
 require_relative 'bot_sel'
 
-driver = Selenium::WebDriver.for :firefox
-driver.manage.timeouts.implicit_wait = 2
-
 argument = {} 
+argument[:affiche_log] = true
+argument[:port] = 4444
+
+current = nil
 ARGV.each do |a|
-  argument[a.intern] = true
+	if current != nil
+		argument[current] = a
+		current = nil
+	elsif a == "-help" or a == "--help" or a == "-h" or a == "--h"
+		puts "run.rb [-port 4444]"
+		exit
+	elsif a == "-port"
+		current = :port
+	else
+		argument[a.intern] = true
+	end
 end
 
-argument[:affiche_log] = true
+#utile pour le developement mais ne fonctionne pas sur un serveur
+#driver = Selenium::WebDriver.for :firefox
+
+#fonctionne sur un serveur mais est lier a un un server selenium
+caps = Selenium::WebDriver::Remote::Capabilities.htmlunit(:javascript_enabled => true)
+driver = Selenium::WebDriver.for :remote, :url => "http://localhost:#{argument[:port]}/wd/hub", :desired_capabilities => caps
+
+driver.manage.timeouts.implicit_wait = 2
+
+
+
 
 bot = Bot.new('./config/skynet', argument, driver)
 bot.run
